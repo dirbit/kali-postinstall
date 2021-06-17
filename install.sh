74f0b5a4dfaf3f9aea5356d67184aba7f87aa2e3
karrni <24398546+karrni@users.noreply.github.com>
2021-06-17 23:11:48 +0200

    add installation script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if ! [ $(id -u) = 0 ]; then
+    echo "Needs to be run as root"
+    exit 1
+fi
+
+# Install i3-gaps and all dependencies/related tools
+apt-get install -y kali-desktop-i3-gaps i3blocks rofi feh
+
+# Install Alacritty
+apt-get install -y cargo cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+git clone https://github.com/jwilm/alacritty
+cd alacritty
+cargo build --release
+cp target/release/alacritty /usr/local/bin
+cd ..
+rm -rf alacritty
