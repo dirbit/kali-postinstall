54b8e505d5494f9c22a980294d3d3c5b52db3618
karrni <24398546+karrni@users.noreply.github.com>
2021-06-19 20:27:38 +0200

    stuff

--- install.sh
+++ install.sh
@@ -5,31 +5,63 @@ if ! [ $(id -u) = 0 ]; then
     exit 1
 fi
 
-cd ..
-mv kali-postinstall $HOME
-cd $HOME
-cd kali-postinstall
-chown root:root . -R
+# Determine the path of the repository
+BASE="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-# Get the root user back
-echo "Set the new root password"
-passwd
+
+# Enable root and set default password
 apt-get install -y kali-root-login
+echo 'root:a' | chpasswd
 
-# Install additional stuff
-apt-get install -y lxappearance nvim
-
-# Install i3-gaps and all dependencies/related tools
+# Install i3-gaps w/ dependencies and related tools
 apt-get install -y kali-desktop-i3-gaps i3blocks rofi feh compton
 
-# Install Alacritty
-apt-get install -y cargo cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
-git clone https://github.com/jwilm/alacritty
-cd alacritty
-cargo build --release
-cp target/release/alacritty /usr/local/bin
-cd ..
-rm -rf alacritty
+# Install general tools
+apt-get install -y lxappearance htop nvim
 
-# Copy dotfiles
-cp -rT dotfiles $HOME
+# Install Alacritty if it does not exist
+if ! [ -x /usr/local/bin/alacritty ]; then
+    # Install dependencies
+    apt-get install -y cargo cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+
+    # Clone and build from source
+    git clone https://github.com/jwilm/alacritty /opt/alacritty
+    cd /opt/alacritty
+    cargo build --release
+    cp target/release/alacritty /usr/local/bin
+    cd $OLDPWD
+fi
+
+# Copy dotfiles, wallpaper and so on
+cp -rT $BASE/home $HOME
+
+# Install additional tools
+# apt
+apt-get install -y seclists gobuster httpie chromium eyewitness bloodhound dbeaver pwncat
+
+# pip
+apt-get install -y python3 python3-pip python3-venv
+pip3 install pipx
+
+pipx install git+https://github.com/cddmp/enum4linux-ng
+pipx install git+https://github.com/dirkjanm/adidnsdump
+pipx install git+https://github.com/bitsadmin/wesng
+
+# go
+apt-get install -y golang
+
+go get github.com/ropnop/kerbrute
+
+# git
+cd /opt
+git clone https://github.com/diego-treitos/linux-smart-enumeration
+git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite
+git clone https://github.com/samratashok/nishang
+
+git clone https://github.com/knavesec/Max
+ln -s /opt/Max/max /usr/sbin/max
+chmod +x /usr/sbin/max
+
+wget https://github.com/ropnop/go-windapsearch/releases/download/v0.3.0/windapsearch-linux-amd64
+mv windapsearch-linux-amd64 /usr/local/bin/windapsearch
+
